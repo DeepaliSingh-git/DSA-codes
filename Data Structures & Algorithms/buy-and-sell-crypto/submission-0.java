3883b846bdefd9690d8b1942ea76af04b5ff2050
Deepali Singh <thebluelightcanvas@gmail.com>
2026-04-27 14:37:13 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy= prices[0];
+        int profit=0;
+        for(int a: prices){
+            buy= Math.min(buy,a);
+            profit= Math.max(profit, a-buy);
+        }
+        return profit;
+    }
+}
