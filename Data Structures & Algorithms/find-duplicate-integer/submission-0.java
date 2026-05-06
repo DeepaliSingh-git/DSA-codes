b337305d84bfa55ea75d9e711bbe8b00a35a00a3
Deepali Singh <thebluelightcanvas@gmail.com>
2026-05-06 17:26:13 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int a:nums){
+            if(set.contains(a)){
+                return a;
+            }
+            set.add(a);
+        }
+        return -1;
+    }
+}
